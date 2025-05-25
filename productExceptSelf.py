3c8137642b0df81702bdea1adb9f6ccf60dcd534
Waled <waled@example.com>
2025-05-25 03:33:58 +0300

    productExceptSelf

---
+++ productExceptSelf.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left_mul = 1
+        right_mul = 1
+        left = []
+        right = [0 for _ in range(len(nums))]
+        for i in range(len(nums)):
+            left_mul = left_mul * nums[i]
+            left.append(left_mul)
+        for i in range(len(nums)-1,-1,-1):
+            right_mul = right_mul * nums[i]
+            right[i] = right_mul
+        res = []
+        for i in range(len(nums)):
+            if i == 0:
+                res.append(right[i])
+            elif i == len(nums)-1:
+                res.append(left[i-1])
+            else:
+                res.append(left[i-1]*right[i+1])
+        return res
+        
