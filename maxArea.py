a6c22894bdbb316de393c59ca78ade44bd7cd05b
Waled <waled@example.com>
2025-05-27 21:39:34 +0300

    maxArea

--- maxArea.py
+++ maxArea.py
@@ -11,10 +11,7 @@ class Solution(object):
             m = max(m, min(height[l], height[r]) * (r - l))
             if height[l] < height[r]:
                 l+=1
-            elif height[l] > height[r]:
-                r-=1
             else:
-                l+=1
                 r-=1
         return m
         
